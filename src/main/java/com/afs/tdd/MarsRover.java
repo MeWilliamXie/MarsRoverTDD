0464b95c6502ab1de68fd874cfa30d57e8d2c008
WILLIAM XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEWI3@OOCL.COM>
2022-07-21 16:47:22 +0800

    feat: finished MarsRover & move & turnLeft & turnRight

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,18 +1,76 @@
 package com.afs.tdd;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MarsRover {
     private static final String locationFormat = "location: (x: %d, y: %d), direction: %s";
-    public MarsRover(int x, int y, String dirction) {
+    private static final List<String> directionList = Arrays.asList("N", "E", "S", "W");
+
+    private int xLocation;
+    private int yLocation;
+    private int directionIndex;
+
+    public MarsRover(int xLocation, int yLocation, String dirction) {
+        this.xLocation = xLocation;
+        this.yLocation = yLocation;
+        this.directionIndex = directionList.indexOf(dirction);
     }
 
-
-    public void executeCommand(String m) {
-
+    public void executeCommand(String command) {
+        switch (command) {
+            case "M":
+                this.move();
+                break;
+            case "L":
+                this.turnLeft();
+                break;
+            case "R":
+                this.turnRight();
+                break;
+        }
     }
+
+    private void move() {
+        switch (this.directionIndex) {
+            case 0:
+                this.yLocation++;
+                break;
+            case 1:
+                this.xLocation++;
+                break;
+            case 2:
+                this.yLocation--;
+                break;
+            case 3:
+                this.xLocation--;
+                break;
+        }
+    }
+
+    private void turnLeft() {
+        this.directionIndex = this.directionIndex - 1 >= 0 ? this.directionIndex - 1 : 3;
+    }
+
+    private void turnRight() {
+        this.directionIndex = this.directionIndex + 1 >= 0 ? this.directionIndex - 1 : 3;
+    }
+
 
     public String getLocation() {
-
-        return null;
+        return String.format(locationFormat, this.xLocation, this.yLocation, directionList.get(this.directionIndex));
     }
 
+    public int getxLocation() {
+        return xLocation;
+    }
+
+    public int getyLocation() {
+        return yLocation;
+    }
+
+    public int getDirectionIndex() {
+        return directionIndex;
+    }
 }
--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -16,4 +16,28 @@ class MarsRoverTest {
         // then
         assertEquals("location: (x: 0, y: 1), direction: N", marsRover.getLocation());
     }
+
+    @Test
+    void should_get_to_0_0_W_when_execute_command_L_given_0_0_N() {
+        // given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+
+        // when
+        marsRover.executeCommand("L");
+
+        // then
+        assertEquals("location: (x: 0, y: 0), direction: W", marsRover.getLocation());
+    }
+
+    @Test
+    void should_get_to_0_0_E_when_execute_command_R_given_0_0_N() {
+        // given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+
+        // when
+        marsRover.executeCommand("R");
+
+        // then
+        assertEquals("location: (x: 0, y: 0), direction: E", marsRover.getLocation());
+    }
 }
